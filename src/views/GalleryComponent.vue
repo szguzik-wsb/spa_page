4770c95718bf6c47efbd966475d7da3204d424da
Szymon Guzik <szymon.guzik@gmail.com>
2022-12-17 17:31:52 +0100

    Galeria - fetch+ hr + v-for

--- src/views/GalleryComponent.vue
+++ src/views/GalleryComponent.vue
@@ -1,8 +1,15 @@
 <template>
   <section id="gallery__component">
     <h1>Galeria</h1>
-    <hr>
+    <hr first>
     <input v-model="findInSpace"> <button @click="getDataFromNasa">Odszukaj w kosmosie</button><br>
+    <p v-if="loader">Pobieram dane...</p>
+    <ul>
+      <li v-for="item in spaceArray" :key="item.data[0].nasa_id">
+        <p>{{ item.data[0].description }}</p>
+        <hr>
+      </li>
+    </ul>
   </section>
 </template>
 
@@ -11,23 +18,29 @@ import axios from "axios";
 
 export default {
   name: "GalleryComponent",
+  created() {
+    // this.getDataFromNasa()
+  },
   data() {
     return {
+      loader: false,
       findInSpace: '',
-      spaceArray: []
+      spaceArray: null
     }
   },
 
   methods: {
     async getDataFromNasa() {
+      this.loader = true
+      this.spaceArray = null
       await axios.get('https://images-api.nasa.gov/search?q=' + this.findInSpace)
-          .then(function (response) {
-            let tmpData = response.data;
-            console.log(tmpData);
+          .then((response) => {
+            this.spaceArray = response.data.collection.items
+            this.loader = false
+            console.log(this.spaceArray)
           })
-          .catch(function (error) {
-            console.log(error);
-          });
+          .catch((error) => {
+          })
     }
   }
 }
@@ -36,5 +49,9 @@ export default {
 <style scoped>
 hr {
   border-top: 1px solid red;
+}
+
+hr[first] {
+  border-top: 1px solid green;
 }
 </style>
